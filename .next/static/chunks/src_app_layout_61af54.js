5b3751836a147d3691550746f67ed4a65e37bb38
Ram Chandel <ram8176970175@gmail.com>
2025-03-27 11:39:42 +0530

    Code clean up

--- .next/static/chunks/src_app_layout_61af54.js
+++ .next/static/chunks/src_app_layout_61af54.js
@@ -5,7 +5,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_layout_61af54.js",
   "chunks": [
-    "static/chunks/node_modules_7325fa._.js",
+    "static/chunks/node_modules_b8b5b2._.js",
     "static/chunks/src_fbce8d._.js",
     "static/chunks/src_app_globals_b80590.css"
   ],
